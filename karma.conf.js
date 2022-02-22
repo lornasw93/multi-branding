fe664b96e8fb57aa6ad6ac7a5664d3a2853d5040
Lorna Watson <lorna.watson@smoothwall.com>
2022-02-22 12:52:11 +0000

    branding changes

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/white-label'),
+      dir: require('path').join(__dirname, './coverage/simple-website'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'white-label'`, () => {
+  it(`should have as title 'simple-website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('white-label');
+    expect(app.title).toEqual('simple-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('white-label app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('simple-website app is running!');
   });
 });
